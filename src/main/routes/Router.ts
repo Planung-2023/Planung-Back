5739244bc3f1552c23ec12658dfbdba1c3ef8898
FranMendo2002 <franmendozita123@gmail.com>
2023-08-04 13:07:37 -0300

    Add files via upload

--- src/main/routes/Router.ts
+++ src/main/routes/Router.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 import { WelcomeRoutes } from "./routers/WelcomeRoutes";
 import { ParticipantesRoutes } from "./routers/ParticipantesRoutes";
-import { Usuario } from "../models/entities/persona/Usuario";
 import { UsuariosRoutes } from "./routers/UsuariosRoutes";
 import { EventosRoutes } from "./routers/EventosRoutes";
+import { RolesRoutes } from "./routers/RolesRoutes";
+import { PermisosRoutes } from "./routers/PermisosRoutes";
+import { UbicacionRoutes } from "./routers/UbicacionRoutes";
 
 export class Router {
 
@@ -16,7 +18,9 @@ export class Router {
         this.app.use("/participantes", ParticipantesRoutes.router);
         this.app.use("/usuarios", UsuariosRoutes.router);
         this.app.use("/eventos", EventosRoutes.router);
-        
+        this.app.use("/roles", RolesRoutes.router);
+        this.app.use("/permisos", PermisosRoutes.router);
+        this.app.use("/ubicaciones", UbicacionRoutes.router);
 
     }
 }
