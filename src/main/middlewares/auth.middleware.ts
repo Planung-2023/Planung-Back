ba751c4a75c23c450298f98fb92f5a4098a943e2
franmendo <franmendozita123@gmail.com>
2023-10-06 12:18:07 -0300

    cambios

--- src/main/middlewares/auth.middleware.ts
+++ src/main/middlewares/auth.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { post } from "superagent";
-//
+
 export const authValidation = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const authorizationHeader = req.headers.authorization;
