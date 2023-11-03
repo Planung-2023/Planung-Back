88121106b484a98deb3fc81761539567b579708e
Tomaskatz47 <68217919+Tomaskatz47@users.noreply.github.com>
2023-11-03 12:53:03 -0300

    Update EventosApiController.ts

--- src/main/controllers/api/eventos/EventosApiController.ts
+++ src/main/controllers/api/eventos/EventosApiController.ts
@@ -1,72 +1,95 @@
 import { NextFunction, Request, Response } from "express";
 import { Evento } from "../../../models/entities/evento/Evento";
+import { Ubicacion } from "../../../models/entities/otros/Ubicacion";
 import { Database } from "../../../server/Database";
+import { RecursosApiController } from "../recursos/RecursosApiController";
+import { UbicacionApiController } from "../ubicacion/UbicacionApiController";
+import { Asistente } from "../../../models/entities/asistencia/Asistente";
+import { Participante } from "../../../models/entities/persona/Participante";
 
+
+//con el id de usuario. Primero obtener el participante, con el participante, en la tabla asistente, 
+//todos los id evento donde el participante id es el usuario
+//Cada Asistente tiene un evento_id y un participante_id. Cada participante tiene un usuario_id. Cada usuario tiene un id. 
+//Pero cada evento tiene un usuario_id. Crear un queryBuilder en el que devuelva los eventos a los que pertenece cada usuario 
 export class EventosApiController {
-    // TODO: Crear o injectar en el constructor el repositorio de eventos asi no se repite el codigo de
-    // getRepository cada vez que lo tengamos que usar, y todo el Controller tiene el repository
-
-    // TODO: Crear o injectar en el constructor el repositorio de eventos asi no se repite el codigo de
-    // getRepository cada vez que lo tengamos que usar, y todo el Controller tiene el repository
-
     public static async index(req: Request, res: Response, next: NextFunction) {
-        try {   
-            const idUsuario = req.query.usuario_id;
-            const eventoRepository = Database.em.getRepository("evento")
+        try {
+            const idUsuario = req.query.usuario_id as string;
+            const participante = await Database.em.findOneBy(Participante, { usuario: { id: idUsuario } })
+            const idParticipante= participante?.id;
+            const asistente = await Database.em.findOneBy(Asistente, { participante: { id: participante?.id } })
+            const idAsistente = asistente?.id;
+            const eventoRepository = Database.em.getRepository("evento");
             const eventos = await eventoRepository
-				.createQueryBuilder("evento")
+                .createQueryBuilder("evento")
+                //"participante.usuario.id", "asistente.id"
                 .select(["evento", "ubicacion", "usuario.id", "usuario.nombreUsuario"])
                 .addSelect("eventoAnterior")
-                .where("evento.creador.id = :idUsuario", { idUsuario })
+                //.where("evento.creador.id = :idUsuario", { idUsuario })
+                .where(":idUsuario= :participanteId", { idUsuario, idParticipante })
+                .andWhere(":idParticipante=:idAsistente", {idParticipante, idAsistente})
+                .andWhere()
+                //.leftJoin("")
                 .leftJoin("evento.ubicacion", "ubicacion")
                 .leftJoin("evento.creador", "usuario")
                 .leftJoin("evento.eventoAnterior", "eventoAnterior")
-				.getMany();;
-        }
-        catch (e) {
+                .getMany();
+            res.json(
+                eventos
+            );
+        } catch (e) {
             next(e);
         }
     }
     public static async show(req: Request, res: Response, next: NextFunction) {
         try {
-            const eventos= await Database.em.findOneBy(Evento, {
-                id: req.params.id
+            const eventos = await Database.em.findOneBy(Evento, {
+                id: req.params.id,
             });
 
-            if(eventos === null) {
+            if (eventos === null) {
                 res.status(404);
                 res.send();
                 return;
             }
             res.json(eventos);
-        }
-        catch (e) {   
+        } catch (e) {
             next(e);
         }
-
     }
+
     public static async store(req: Request, res: Response, next: NextFunction) {
         try {
-            const evento = new Evento();
+            const { evento } = req.body;
+            const { ubicacion } = evento;
 
-            EventosApiController.asignarParametros(evento!!, req.body);
+            const ubicacionDb = new Ubicacion();
+            UbicacionApiController.asignarParametros(ubicacionDb, ubicacion);
+            await Database.em.save(ubicacionDb);
 
-            await Database.em.save(evento);
+            const eventoDb = new Evento();
+            eventoDb.setUbicacion(ubicacionDb);
+            EventosApiController.asignarParametros(eventoDb!!, evento);
 
-            res.status(200);
-            res.send();
-        }
-        catch (e) {
+            await Database.em.save(eventoDb);
+            RecursosApiController.crearRecursos(evento.recursos, eventoDb.id);
+
+            res.status(201).send({
+                evento: eventoDb,
+            });
+        } catch (e) {
             next(e);
         }
     }
+
     public static async update(req: Request, res: Response, next: NextFunction) {
         try {
             const eventos = await Database.em.findOneBy(Evento, {
-                id: req.params.id
+                id: req.params.id,
             });
 
-            if(eventos === null) {
+            if (eventos === null) {
                 res.status(404);
                 res.send();
                 return;
@@ -78,18 +101,17 @@ export class EventosApiController {
 
             res.status(200);
             res.send();
-        }
-        catch (e) {
+        } catch (e) {
             next(e);
         }
     }
     public static async remove(req: Request, res: Response, next: NextFunction) {
         try {
             const eventos = await Database.em.findOneBy(Evento, {
-                id: req.params.id
+                id: req.params.id,
             });
 
-            if(eventos === null) {
+            if (eventos === null) {
                 res.status(404);
                 res.send();
                 return;
@@ -98,33 +120,24 @@ export class EventosApiController {
             await Database.em.remove(eventos);
             res.status(200);
             res.send();
-        }
-        catch (e) {
+        } catch (e) {
             next(e);
         }
-    
     }
-    
+
     private static asignarParametros(evento: Evento, params: any) {
-        //const formatHour = EventosApiController.formatHour(params.horaInicio);
-
-		evento.setNombre(params.nombre);
-		evento.setRecursos(params.recursos); // No va. RecursosController.  /eventos/1/recursos/ POST body
-		evento.setUbicacion(params.ubicacion); // No va. UbicacionController
-		evento.setFecha(params.fecha);
-		evento.setEstadoEvento(params.estadoEvento);
-		evento.setEventoAnterior(params.eventoAnterior);
-		evento.setAsistentes(params.asistentes);
-		evento.setEsVisible(params.esVisible);
-        
-        evento.setHoraInicio(new Date(`1970-01-01T${params.horaInicio}Z`));
+        evento.setNombre(params.nombre);
+        evento.setFecha(params.fecha);
+        evento.setEstadoEvento(params.estadoEvento ? params.estadoEvento : null);
+        evento.setEventoAnterior(params.eventoAnterior ? params.eventoAnterior : null);
+        evento.setEsVisible(params.esVisible);
+        evento.setHoraInicio(params.horaInicio);
         evento.setHoraFin(params.horaFin ? params.horaFin : null);
-        evento.setTipoEvento(params.tipoEvento)
-        evento.setTipoInvitacion(params.tipoInvitacion)
-
+        evento.setTipoEvento(params.tipoEvento ? params.tipoEvento : null);
+        evento.setTipoInvitacion(params.tipoInvitacion ? params.tipoInvitacion : null);
         evento.setDescripcion(params.descripcion);
-	}
-    
+    }
+
     public static async findOneById(id: string) {
         const evento = await Database.em.findOneBy(Evento, { id });
 
