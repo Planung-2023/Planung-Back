1bf9bc80d14624b09bb74689b71bffba3a7cd590
tomyk <tomykatz@gmail.com>
2023-11-05 17:05:19 -0300

    index

--- src/main/controllers/api/eventos/EventosApiController.ts
+++ src/main/controllers/api/eventos/EventosApiController.ts
@@ -6,38 +6,32 @@ import { RecursosApiController } from "../recursos/RecursosApiController";
 import { UbicacionApiController } from "../ubicacion/UbicacionApiController";
 import { Asistente } from "../../../models/entities/asistencia/Asistente";
 import { Participante } from "../../../models/entities/persona/Participante";
+import { MoreThanOrEqual, LessThan, IsNull, Brackets } from "typeorm";
 
 
-//con el id de usuario. Primero obtener el participante, con el participante, en la tabla asistente, 
-//todos los id evento donde el participante id es el usuario
-//Cada Asistente tiene un evento_id y un participante_id. Cada participante tiene un usuario_id. Cada usuario tiene un id. 
-//Pero cada evento tiene un usuario_id. Crear un queryBuilder en el que devuelva los eventos a los que pertenece cada usuario 
 export class EventosApiController {
     public static async index(req: Request, res: Response, next: NextFunction) {
         try {
-            const idUsuario = req.query.usuario_id as string;
-            const participante = await Database.em.findOneBy(Participante, { usuario: { id: idUsuario } })
-            const idParticipante= participante?.id;
-            const asistente = await Database.em.findOneBy(Asistente, { participante: { id: participante?.id } })
-            const idAsistente = asistente?.id;
-            const eventoRepository = Database.em.getRepository("evento");
+            const idUsuario = req.query.usuario_id;
+            const eventoRepository = Database.em.getRepository(Evento);
             const eventos = await eventoRepository
-                .createQueryBuilder("evento")
-                //"participante.usuario.id", "asistente.id"
-                .select(["evento", "ubicacion", "usuario.id", "usuario.nombreUsuario"])
-                .addSelect("eventoAnterior")
-                //.where("evento.creador.id = :idUsuario", { idUsuario })
-                .where(":idUsuario= :participanteId", { idUsuario, idParticipante })
-                .andWhere(":idParticipante=:idAsistente", {idParticipante, idAsistente})
-                .andWhere()
-                //.leftJoin("")
-                .leftJoin("evento.ubicacion", "ubicacion")
-                .leftJoin("evento.creador", "usuario")
-                .leftJoin("evento.eventoAnterior", "eventoAnterior")
-                .getMany();
-            res.json(
-                eventos
-            );
+            .createQueryBuilder("evento")
+            .select([
+                "evento"
+            ])
+            .innerJoin("evento.asistentes", "asistente")
+            .innerJoin("asistente.participante", "participante")
+            .innerJoin("participante.usuario", "usuario")
+            .where("usuario.id = :idUsuario", { idUsuario })
+            .andWhere("evento.fecha >= :fechaActual", { fechaActual: new Date() })
+            .andWhere(
+                new Brackets(qb => {
+                    qb.where("evento.fecha > :fechaActual", { fechaActual: new Date() }) // Eventos después de la fecha actual
+                        .orWhere("evento.horaInicio >= :horaActual", { horaActual: new Date() }); // Eventos en o después de la hora actual
+                })
+            )
+            .getMany();;
+        res.json(eventos);
         } catch (e) {
             next(e);
         }
