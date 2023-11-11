beab28217159ac165ea0a8b532b40217fdc5b232
FranMendo2002 <franmendozita123@gmail.com>
2023-11-11 19:19:57 -0300

    Usuario se registra al tener Auth Validation

--- src/main/controllers/helpers/GetAuthUser.ts
+++ src/main/controllers/helpers/GetAuthUser.ts
@@ -1,36 +1,15 @@
 import { Request } from "express";
-import { Database } from "../../server/Database";
 import { Usuario } from "../../models/entities/persona/Usuario";
-import { FactoryUsuario } from "../../models/factories/FactoryUsuario";
-import { FactoryParticipante } from "../../models/factories/FactoryParticipante";
+import { Database } from "../../server/Database";
 
 export const getAuthUser = async (req: Request) => {
+    const userAuth0 = JSON.parse(req.headers["user"] as string);
 
-        console.log(req.headers["user"]);
-        const userAuth0 = JSON.parse(req.headers["user"] as string);
-
-        let user = await Database.em.findOne(Usuario, { where: {
+    let user = await Database.em.findOne(Usuario, {
+        where: {
             email: userAuth0.email,
-        }})
+        },
+    });
 
-        if(user === null || user === undefined) {
-            user = await FactoryUsuario.crearUsuario({
-                auth0Id: userAuth0.sub,
-                nombre: userAuth0.given_name,
-                apellido: userAuth0.family_name,
-                email: userAuth0.email,
-                usuario: userAuth0.nickname,
-            });
-
-            const participante = await FactoryParticipante.crearParticipante({
-                nombre: userAuth0.given_name,
-                apellido: userAuth0.family_name,
-                email: userAuth0.email,
-                usuario: user
-            });
-
-            await Database.em.save(user);
-            await Database.em.save(participante);
-        }
-        return user;
+    return user!;
 };
--- src/main/middlewares/auth.middleware.ts
+++ src/main/middlewares/auth.middleware.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import { post } from "superagent";
+import { Usuario } from "../models/entities/persona/Usuario";
+import { FactoryParticipante } from "../models/factories/FactoryParticipante";
+import { FactoryUsuario } from "../models/factories/FactoryUsuario";
+import { Database } from "../server/Database";
 
 export const authValidation = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -17,7 +21,36 @@ export const authValidation = async (req: Request, res: Response, next: NextFunc
         if (response.status >= 400) {
             return res.status(401).json({ msg: "Token de autorización no válido" });
         }
+
         req.headers["user"] = JSON.stringify(response.body);
+
+        const userAuth0 = response.body;
+
+        let user = await Database.em.findOne(Usuario, {
+            where: {
+                email: userAuth0.email,
+            },
+        });
+
+        if (user === null || user === undefined) {
+            user = await FactoryUsuario.crearUsuario({
+                auth0Id: userAuth0.sub,
+                nombre: userAuth0.given_name,
+                apellido: userAuth0.family_name,
+                email: userAuth0.email,
+                usuario: userAuth0.nickname,
+            });
+
+            const participante = await FactoryParticipante.crearParticipante({
+                nombre: userAuth0.given_name,
+                apellido: userAuth0.family_name,
+                email: userAuth0.email,
+                usuario: user,
+            });
+
+            await Database.em.save(user);
+            await Database.em.save(participante);
+        }
 
         next();
     } catch (error) {
