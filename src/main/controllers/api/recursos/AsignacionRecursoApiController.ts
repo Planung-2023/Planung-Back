cb314282a91949f59247583550b89a768dffe111
FranMendo2002 <franmendozita123@gmail.com>
2023-11-19 15:53:49 -0300

    Asignacion recurso no requiere permisos de admin

--- src/main/controllers/api/recursos/AsignacionRecursoApiController.ts
+++ src/main/controllers/api/recursos/AsignacionRecursoApiController.ts
@@ -3,30 +3,16 @@ import { Asistente } from "../../../models/entities/asistencia/Asistente";
 import { AsignacionRecurso } from "../../../models/entities/recursos/AsignacionRecurso";
 import { Recurso } from "../../../models/entities/recursos/Recurso";
 import { Database } from "../../../server/Database";
-import { getAuthUser } from "../../helpers/GetAuthUser";
-import { AsistentesApiController } from "../personas/AsistentesApiController";
 
 export class AsignacionRecursoApiController {
     public static async store(req: Request, res: Response, next: NextFunction) {
         try {
             const { recursoId, asistenteId } = req.body;
-            const usuario = await getAuthUser(req);
+
             const recurso = await Database.em.findOneBy(Recurso, { id: recursoId });
-            const asistente = await Database.em
-                .getRepository(Asistente)
-                .createQueryBuilder("asistente")
-                .leftJoinAndSelect("asistente.evento", "evento")
-                .where("asistente.id = :asistenteId", { asistenteId })
-                .getOne();
+            const asistente = await Database.em.findOneBy(Asistente, { id: asistenteId });
 
             if (!asistente || !recurso) return res.status(404).send();
-
-            const asistenteAdministrador = await AsistentesApiController.getAsistenteAdministrador(
-                usuario,
-                asistente.evento.id,
-            );
-            if (!asistenteAdministrador)
-                return res.status(401).json({ msg: "No es administrador" }).send();
 
             const asignacionRecurso = new AsignacionRecurso();
             AsignacionRecursoApiController.asignarParametros(asignacionRecurso, req.body);
@@ -40,7 +26,6 @@ export class AsignacionRecursoApiController {
 
     public static async update(req: Request, res: Response, next: NextFunction) {
         try {
-            const usuario = await getAuthUser(req);
             const { idAsignacion } = req.params;
             const asignacionRecurso = await Database.em.findOneBy(AsignacionRecurso, {
                 id: idAsignacion,
@@ -48,22 +33,6 @@ export class AsignacionRecursoApiController {
 
             if (!asignacionRecurso)
                 return res.status(404).json({ msg: "AsignacionRecurso not found" }).send();
-
-            const asistente = await Database.em
-                .getRepository(Asistente)
-                .createQueryBuilder("asistente")
-                .leftJoinAndSelect("asistente.evento", "evento")
-                .where("asistente.id = :asistenteId", {
-                    asistenteId: asignacionRecurso.asistente.id,
-                })
-                .getOne();
-
-            const asistenteAdministrador = await AsistentesApiController.getAsistenteAdministrador(
-                usuario,
-                asistente!.evento.id,
-            );
-            if (!asistenteAdministrador)
-                return res.status(401).json({ msg: "No es administrador" }).send();
 
             AsignacionRecursoApiController.asignarParametros(asignacionRecurso, req.body);
             await Database.em.save(asignacionRecurso);
@@ -83,5 +52,6 @@ export class AsignacionRecursoApiController {
         );
         asignacionRecurso.setRecurso(params.recursoId);
         asignacionRecurso.setAsistente(params.asistenteId);
+        asignacionRecurso.setFechaHora(new Date());
     }
 }
