a7eb55e607a71d93648db0159214d7c5d2edc0b4
Tomaskatz47 <68217919+Tomaskatz47@users.noreply.github.com>
2023-11-22 22:25:30 -0300

    cambiarEstadoAceptacion

--- src/main/routes/routers/AsistentesRoutes.ts
+++ src/main/routes/routers/AsistentesRoutes.ts
@@ -7,6 +7,7 @@ export class AsistentesRoutes {
     static {
         this.router = Router();
 
+        this.router.put('/:id/cambiar-aceptacion', AsistentesApiController.cambiarEstadoAceptacion);
         this.router.delete('/:id', AsistentesApiController.removeAsistenteById);
     }
 }    
